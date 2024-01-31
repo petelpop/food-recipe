7406757688153af4e342cc6e44131af9dd2e774f
ipal <72238450+petelpop@users.noreply.github.com>
2024-01-31 13:49:33 +0700

    create appbar

--- lib/views/widgets/recipe_card.dart
+++ lib/views/widgets/recipe_card.dart
@@ -11,7 +11,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(width: 10),
+            Text('Food Recipe')
+          ],
+        ),
+      ),
     );
   }
 }
